06c6da55160c2e27a95a22cbd1e1fcaa2749744e
EliasVincent <riseyeti@outlook.de>
2023-01-08 00:36:58 +0100

    asset creation

---
+++ actors/ammo_dispenser.gd
@@ -0,0 +1,25 @@
+extends StaticBody3D
+
+@onready var dispense_area = $DispenseArea
+
+@export var AmmoToDispense: int = 5
+
+var canDispense: bool = true
+
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+
+func _on_dispense_area_body_entered(body):
+	if body.is_in_group("PLAYER") and canDispense:
+		canDispense = false
+		Globals.sprayAmmo += AmmoToDispense
+		
+
+
+func _on_dispenser_cooldown_timeout():
+	canDispense = true
